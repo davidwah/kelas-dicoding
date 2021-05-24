ea2aaabb744ce16a2ef1366954a906c22973d813
David Wahyu P <22112331+davidwah@users.noreply.github.com>
2021-05-24 13:40:17 +0700

    add kumpul

---
+++ dasar-pemrograman-javascript/kumpul/kuis1_Kuis_Coding_Variabel_dan_Tipe_Data.js
@@ -0,0 +1,23 @@
+// Kuis Coding : Variabel dan Tipe Data
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+
+/**
+ * Hiraukan kode di bawah ini
+ */
+firstName = "David Wahyu";
+lastName = "Pratomo";
+age = 25;
+isMarried = false
+
+module.exports = {
+    firstName, lastName, age, isMarried,
+};
