93bc26acc3b3197f278fec0ca4c975e06f2a01d3
David Wahyu P <22112331+davidwah@users.noreply.github.com>
2021-06-10 11:25:27 +0700

    create web-service

---
+++ membuat_aplikasi_backend_pemula/web-service-nodejs/server.js
@@ -0,0 +1,47 @@
+const http = require("http");
+
+const requestListener = (request, response) => {
+    response.setHeader("Content-Type", "text/html");
+    response.statusCode = 200;
+
+    const { method, url } = request;
+
+    if(url === '/') {
+        // TODO 2: Logika respons bila url bernilai '/'
+        if(method === 'GET') {
+            // Respons bila client menggunakan GET
+            response.end('<h1>Ini adalah homepage</h1>');
+        } else {
+            // Respons bila client tidak menggunakan GET
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            // Respons bila client menggunakan GET
+            response.end('<h1>Halo! Ini adalah halaman about</h1>')
+        } else if (method === 'POST') {
+            // respons bila client menggunakan POST
+            let body = [];
+            request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const {name} = JSON.parse(body);
+            response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+        });
+      } else {
+        response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+      }
+    }
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
